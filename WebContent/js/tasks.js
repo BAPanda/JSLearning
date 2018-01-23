edcca860a1ed303b145eae1ae19bf6dc362939c8
Anastasia Kuzmina <akuzmina@neoflex.ru>
2018-01-23 17:32:16 +0400

    finished js for adding new categories (without database)

--- WebContent/js/tasks.js
+++ WebContent/js/tasks.js
@@ -1,14 +1,51 @@
+function addSubcategory() {
+	var ul;
+	
+	if (this.parentElement.lastElementChild.tagName == 'ul') {
+		ul = this.parentElement.lastElementChild;
+	} else {
+		ul = document.createElement('ul');
+		this.parentElement.appendChild(ul);
+	}
+	
+    var newLi = document.createElement('li');
+	
+	newLi.id = 'li-n-' + (ul.childNodes.length);
+	
+	newLi.innerHTML = prompt('Category name','');
+	
+	ul.appendChild(newLi);
+	
+	createButton(newLi);
+}
+
+function createButton(newLi) {
+	var button = document.createElement('button');
+	
+	button.classList.add('btn');
+	button.classList.add('btn-primary');
+	button.classList.add('btn-sm');
+	button.classList.add('add-btn');
+	
+	newLi.appendChild(button);
+	
+	button.onclick = addSubcategory;
+}
 
 var buttonAdd = document.getElementById('addToList');
 
-buttonAdd.onclick = function () {
+buttonAdd.onclick = function () {	
+	
 	var list = document.getElementById('list');
 	
 	var newLi = document.createElement('li');
 	
-	newLi.id = 'li-n';
-	newLi.innerHtml = '';
+	newLi.id = 'li-n-' + (list.childNodes.length);
+	
+	newLi.innerHTML = prompt('Category name','');
+	
+	createButton(newLi);
 	
 	list.appendChild(newLi);
-	
 }
+
--- target/JSLearning-0.0.1-SNAPSHOT/js/tasks.js
+++ target/JSLearning-0.0.1-SNAPSHOT/js/tasks.js
@@ -1,20 +1,51 @@
-(function() {
-	window.onload = function() {
-		var result = prompt('enter your name', '');
-		document.getElementById("name").innerHTML = 'Hi,' + result;
+function addSubcategory() {
+	var ul;
+	
+	if (this.parentElement.lastElementChild.tagName == 'ul') {
+		ul = this.parentElement.lastElementChild;
+	} else {
+		ul = document.createElement('ul');
+		this.parentElement.appendChild(ul);
 	}
-})();
+	
+    var newLi = document.createElement('li');
+	
+	newLi.id = 'li-n-' + (ul.childNodes.length);
+	
+	newLi.innerHTML = prompt('Category name','');
+	
+	ul.appendChild(newLi);
+	
+	createButton(newLi);
+}
+
+function createButton(newLi) {
+	var button = document.createElement('button');
+	
+	button.classList.add('btn');
+	button.classList.add('btn-primary');
+	button.classList.add('btn-sm');
+	button.classList.add('add-btn');
+	
+	newLi.appendChild(button);
+	
+	button.onclick = addSubcategory;
+}
 
 var buttonAdd = document.getElementById('addToList');
 
-buttonAdd.onclick = function () {
+buttonAdd.onclick = function () {	
+	
 	var list = document.getElementById('list');
 	
 	var newLi = document.createElement('li');
 	
-	newLi.id = 'li-n';
+	newLi.id = 'li-n-' + (list.childNodes.length);
+	
+	newLi.innerHTML = prompt('Category name','');
+	
+	createButton(newLi);
 	
 	list.appendChild(newLi);
-	
-	console.log(newLi);
 }
+
